fc99f5154040400af22b72287438ba862c713858
PongsiriTK <mightycat24@gmail.com>
2025-05-08 14:59:59 +0700

    Update dependencies in pubspec.yaml, add launch configurations for VSCode, and create AppColor class for color management. Added new packages including go_router, dio, bloc, and others to enhance functionality and maintainability.

---
+++ lib/app/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color deepViolet = Color(0xFF4B006E);
+  static const Color mediumPurple = Color(0xFF7B2FF2);
+  static const Color lightPurple = Color(0xFFB266FF);
+  static const Color textPurple = Color(0xFF3D0066);
+  static const Color white = Color(0xFFFFFFFF);
+}
